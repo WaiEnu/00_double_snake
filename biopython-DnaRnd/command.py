b4d920508ed81d136ccb468f546fb160f1138a30
WaiEnu <strayhituj@gmail.com>
2020-02-05 21:52:46 +0900

    20.02.05

--- biopython-DnaRnd/command.py
+++ biopython-DnaRnd/command.py
@@ -1,15 +1,4 @@
 from flask import Flask, render_template, request, Markup, redirect
-import pandas as pd
-from pandas import DataFrame
-import pandas.plotting as matrix
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import seaborn as sns
-from io import BytesIO
-import urllib
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.figure import Figure
-import numpy as np
 import generate
 
 app = Flask(__name__)
@@ -34,12 +23,12 @@ def gen():
 
     data_o = generate.generate_origin(length,iteration)
     data_m = generate.generate_mutant(data_o['DNA'],base,locate,length)
-    table_o = make_table_sec(data_o)
-    table_m = make_table_sec(data_m)
-    read_o = make_read_sec(data_o)
-    read_m = make_read_sec(data_m)
-    graph_o = make_graph_df(data_o)
-    graph_m = make_graph_df(data_m)
+    table_o = Markup(generate.make_table_sec(data_o))
+    table_m = Markup(generate.make_table_sec(data_m))
+    read_o = Markup(generate.make_read_sec(data_o))
+    read_m = Markup(generate.make_read_sec(data_m))
+    graph_o = generate.make_graph_df(data_o)
+    graph_m = generate.make_graph_df(data_m)
     data.origin=graph_m
     data.mutate=graph_o
     data.isNull=False
@@ -48,49 +37,16 @@ def gen():
 @app.route("/plot/origin")
 def make_graph_origin():
     if data.isNull is False:
-        return make_graph_sec(data.origin,'origin')
+        return generate.make_graph_sec(data.origin,'origin')
     else:
         return ''
 
 @app.route("/plot/mutate")
 def make_graph_mutate():
     if data.isNull is False:
-        return make_graph_sec(data.mutate,'mutate')
+        return generate.make_graph_sec(data.mutate,'mutate')
     else:
         return ''
-
-def make_table_sec(table_deta):
-    table_ret_df=DataFrame(table_deta)
-    table_df=table_ret_df[['DNA','mRNA']]
-    table=table_df.to_html(header='true')
-    return Markup(table)
-
-def make_read_sec(read_deta):
-    read_ret_df=DataFrame(read_deta)
-    read_df=read_ret_df[['codon','protain','mRNA']]
-    read=read_df.to_html(header='true')
-    return Markup(read)
-
-def make_graph_df(graph_deta):
-    graph_ret_df=DataFrame(graph_deta)
-    graph_df=graph_ret_df[['STT','STP','LEN']]
-    return graph_df
-
-# 生成した遺伝暗号の情報を見たい
-# → 配列の長さを見よう
-# → 開始位置を割り出せば大体の長さの見当がつく？
-def make_graph_sec(graph_df,title):
-    fig = Figure()
-    fig, axes = plt.subplots(2, 2, figsize=(6, 6))
-    sns.scatterplot(x='STT', y='STP', data=graph_df, ax=axes[0,0])
-    sns.scatterplot(x='STT', y='LEN', data=graph_df, ax=axes[0,1])
-    sns.distplot(graph_df['LEN'], ax=axes[1,:])
-    fig.tight_layout()
-    canvas = FigureCanvasAgg(fig)
-    png_output = BytesIO()
-    canvas.print_png(png_output)
-    img_data = urllib.parse.quote(png_output.getvalue())
-    return img_data
 
 if __name__ == '__main__':
     app.debug = True
--- biopython-DnaRnd/generate.py
+++ biopython-DnaRnd/generate.py
@@ -1,3 +1,14 @@
+import pandas as pd
+from pandas import DataFrame
+import pandas.plotting as matrix
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+import urllib
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import numpy as np
 import genetics
 
 def generate_origin(length,iteration):
@@ -34,3 +45,36 @@ def getTable(master):
 
 def getRow(dic,str_obj):
     return [d.get(str_obj) for d in dic]
+
+def make_table_sec(table_deta):
+    table_ret_df=DataFrame(table_deta)
+    table_df=table_ret_df[['DNA','mRNA']]
+    table=table_df.to_html(header='true')
+    return table
+
+def make_read_sec(read_deta):
+    read_ret_df=DataFrame(read_deta)
+    read_df=read_ret_df[['codon','protain','mRNA']]
+    read=read_df.to_html(header='true')
+    return read
+
+def make_graph_df(graph_deta):
+    graph_ret_df=DataFrame(graph_deta)
+    graph_df=graph_ret_df[['STT','STP','LEN']]
+    return graph_df
+
+# 生成した遺伝暗号の情報を見たい
+# → 配列の長さを見よう
+# → 開始位置を割り出せば大体の長さの見当がつく？
+def make_graph_sec(graph_df,title):
+    fig = Figure()
+    fig, axes = plt.subplots(2, 2, figsize=(6, 6))
+    sns.scatterplot(x='STT', y='STP', data=graph_df, ax=axes[0,0])
+    sns.scatterplot(x='STT', y='LEN', data=graph_df, ax=axes[0,1])
+    sns.distplot(graph_df['LEN'], ax=axes[1,:])
+    fig.tight_layout()
+    canvas = FigureCanvasAgg(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    img_data = urllib.parse.quote(png_output.getvalue())
+    return img_data
