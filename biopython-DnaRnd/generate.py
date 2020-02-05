6adb93a06c1f24ee89acb59cf37dcbc35eb4d828
WaiEnu <strayhituj@gmail.com>
2020-02-05 21:43:24 +0900

    20.02.05

--- biopython-DnaRnd/generate.py
+++ biopython-DnaRnd/generate.py
@@ -4,11 +4,12 @@ def generate_origin(length,iteration):
     ittr = int(iteration)
     lens = int(length)
     origin=[]
-    for i in range(0,ittr):
+    counter = ittr
+    while counter > 0:
         pre_seq=genetics.rndn_sequence(lens)
         origin.extend(genetics.translate(pre_seq))
-    dic = getTable(origin)
-    return dic
+        counter -= 1
+    return getTable(origin)
 
 def generate_mutant(origin,base,locate,length):
     index = int(base)
@@ -18,8 +19,7 @@ def generate_mutant(origin,base,locate,length):
     for sequence in origin:
         pre_seq=genetics.one_flame_shift(sequence,index,start,end)
         mutate.extend(genetics.translate(pre_seq))
-    dic = getTable(mutate)
-    return dic
+    return getTable(mutate)
 
 def getTable(master):
     master_STT=getRow(master,'STT')
@@ -30,9 +30,7 @@ def getTable(master):
     master_DNA=getRow(master,'DNA')
     master_CNA=getRow(master,'cDNA')
     master_RNA=getRow(master,'mRNA')
-    dic = {'STT':master_STT,'STP':master_STP,'LEN':master_LEN,'codon':master_COD,'protain':master_PRO,'DNA':master_DNA,'cDNA':master_CNA,'mRNA':master_RNA}
-    return dic
+    return {'STT':master_STT,'STP':master_STP,'LEN':master_LEN,'codon':master_COD,'protain':master_PRO,'DNA':master_DNA,'cDNA':master_CNA,'mRNA':master_RNA}
 
 def getRow(dic,str_obj):
-    arr = [d.get(str_obj) for d in dic]
-    return arr
+    return [d.get(str_obj) for d in dic]
