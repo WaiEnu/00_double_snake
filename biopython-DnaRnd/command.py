c8af9c46ef31a24d5c1305efb2b45063626d02ed
WaiEnu <strayhituj@gmail.com>
2020-02-07 18:25:06 +0900

    20.02.07

--- biopython-DnaRnd/command.py
+++ biopython-DnaRnd/command.py
@@ -1,5 +1,16 @@
 from flask import Flask, render_template, request, Markup, redirect
-import generate
+import pandas as pd
+from pandas import DataFrame
+import pandas.plotting as matrix
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+from io import BytesIO
+import urllib
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+import numpy as np
+import genetics
 
 app = Flask(__name__)
 
@@ -11,6 +22,8 @@ data=DataStore()
 
 @app.route('/')
 def index():
+    data.origin = None
+    data.mutate = None
     data.isNull=True
     return render_template('index.html',graph_o=None,graph_m=None)
 
@@ -21,32 +34,63 @@ def gen():
     locate = request.form['locate']
     base = request.form['base']
 
-    data_o = generate.generate_origin(length,iteration)
-    data_m = generate.generate_mutant(data_o['DNA'],base,locate,length)
-    table_o = Markup(generate.make_table_sec(data_o))
-    table_m = Markup(generate.make_table_sec(data_m))
-    read_o = Markup(generate.make_read_sec(data_o))
-    read_m = Markup(generate.make_read_sec(data_m))
-    graph_o = generate.make_graph_df(data_o)
-    graph_m = generate.make_graph_df(data_m)
-    data.origin=graph_m
-    data.mutate=graph_o
+    data.origin = genetics.generate_origin(length,iteration)
+    data.mutate = genetics.generate_mutant(genetics.getRow(data.origin,'DNA'),base,locate,length)  
     data.isNull=False
+
+    data_o = DataFrame(data.origin)
+    data_m = DataFrame(data.mutate)
+    table_o = make_table_sec(data_o)
+    table_m = make_table_sec(data_m)
+    read_o = make_read_sec(data_o)
+    read_m = make_read_sec(data_m)
+    graph_o = make_graph_sec(data_o,'origin')
+    graph_m = make_graph_sec(data_m,'mutate')
+
     return render_template('index.html',table_o=table_o,table_m=table_m,read_o=read_o,read_m=read_m,graph_o=graph_o,graph_m=graph_m)
 
 @app.route("/plot/origin")
 def make_graph_origin():
     src="data:image/png:base64," 
     if data.isNull is False:
-        src += generate.make_graph_sec(data.origin,'origin')
+        data_o = DataFrame(data.origin)
+        src += make_graph_sec(data_o,'origin')
     return src
 
 @app.route("/plot/mutate")
 def make_graph_mutate():
     src="data:image/png:base64," 
     if data.isNull is False:
-        src += generate.make_graph_sec(data.mutate,'mutate')
+        data_m =DataFrame(data.mutate)
+        src += make_graph_sec(data_m,'mutate')
     return src
+
+def make_table_sec(table_ret_df):
+    table_df=table_ret_df[['DNA','mRNA']]
+    table=table_df.to_html(header='true')
+    return Markup(table)
+
+def make_read_sec(read_ret_df):
+    read_df=read_ret_df[['codon','protain','mRNA']]
+    read=read_df.to_html(header='true')
+    return Markup(read)
+
+# 生成した遺伝暗号の情報を見たい
+# → 配列の長さを見よう
+# → 開始位置を割り出せば大体の長さの見当がつく？
+def make_graph_sec(graph_ret_df,title):
+    graph_df=graph_ret_df[['STT','STP','LEN']]
+    fig = Figure()
+    fig, axes = plt.subplots(2, 2, figsize=(6, 6))
+    sns.scatterplot(x='STT', y='STP', data=graph_df, ax=axes[0,0])
+    sns.scatterplot(x='STT', y='LEN', data=graph_df, ax=axes[0,1])
+    sns.distplot(graph_df['LEN'], ax=axes[1,:])
+    fig.tight_layout()
+    canvas = FigureCanvasAgg(fig)
+    png_output = BytesIO()
+    canvas.print_png(png_output)
+    img_data = urllib.parse.quote(png_output.getvalue())
+    return img_data
 
 if __name__ == '__main__':
     app.debug = True
--- biopython-DnaRnd/genetics.py
+++ biopython-DnaRnd/genetics.py
@@ -6,6 +6,43 @@ import re
 dna = "AGCT"
 stop_str="@"
 
+def generate_origin(length,iteration):
+    ittr = int(iteration)
+    lens = int(length)
+    origin=[]
+    counter = ittr
+    while counter > 0:
+        pre_seq=rndn_sequence(lens)
+        origin.extend(translate(pre_seq))
+        counter -= 1
+    # return getTable(origin)
+    return origin
+
+def generate_mutant(origin,base,locate,length):
+    index = int(base)
+    start = int(locate)
+    end = int(length)
+    mutate=[]
+    for sequence in origin:
+        pre_seq=one_flame_shift(sequence,index,start,end)
+        mutate.extend(translate(pre_seq))
+    # return getTable(mutate)
+    return mutate
+
+def getTable(master):
+    master_STT=getRow(master,'STT')
+    master_STP=getRow(master,'STP')
+    master_LEN=getRow(master,'LEN')
+    master_COD=getRow(master,'codon')
+    master_PRO=getRow(master,'protain')
+    master_DNA=getRow(master,'DNA')
+    master_CNA=getRow(master,'cDNA')
+    master_RNA=getRow(master,'mRNA')
+    return {'STT':master_STT,'STP':master_STP,'LEN':master_LEN,'codon':master_COD,'protain':master_PRO,'DNA':master_DNA,'cDNA':master_CNA,'mRNA':master_RNA}
+
+def getRow(dic,str_obj):
+    return [d.get(str_obj) for d in dic]
+
 def rndn_sequence(lens, sep=''):
     return sep.join([ choice(dna) for x in range(lens) ])
 
